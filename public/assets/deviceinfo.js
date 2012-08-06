1ba7d4648ab375cef460354ddd942244410029cf
Dave Desrochers <dave726@gmail.com>
2012-08-05 17:47:35 -0700

    Added a few more ways to get size for device info page

--- public/assets/deviceinfo.js
+++ public/assets/deviceinfo.js
@@ -4,6 +4,9 @@
     $('#jquery-document').text($(document).width() + 'x' + $(document).height());
     $('#inner').text(window.innerWidth + 'x' + window.innerHeight);
     $('#screen').text(screen.width + 'x' + screen.height);
+    $('#client').text(document.documentElement.clientWidth + 'x' + document.documentElement.clientHeight)
+    $('#offset').text(document.documentElement.offsetWidth + 'x' + document.documentElement.offsetHeight)
+    $('#page').text(window.pageXOffset + 'x' + window.pageYOffset)
   }
 
   $(window).on('resize', function() { updateSize(); });
